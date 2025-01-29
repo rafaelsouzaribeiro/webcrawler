7253a4705780312a4cd13436f8ec81eaba0c7fd5
Rafael Fernando Souza Ribeiro <rafael.ribeiro@trescon.com.br>
2025-01-29 08:22:40 -0300

    add retrieving the href

--- internal/infra/web/getElementByTag.go
+++ internal/infra/web/getElementByTag.go
@@ -2,9 +2,16 @@ package web
 
 import "github.com/PuerkitoBio/goquery"
 
-func GetElementsByTag(doc *goquery.Document, tag string, receive chan<- string) {
+func GetElementsByTag(doc *goquery.Document, tag string, condition string, receive chan<- string) {
 	doc.Find(tag).Each(func(index int, item *goquery.Selection) {
-		text := item.Text()
-		receive <- text
+		if condition == "text" {
+			text := item.Text()
+			receive <- text
+		} else if condition == "href" {
+			href, exists := item.Attr("href")
+			if exists {
+				receive <- href
+			}
+		}
 	})
 }
